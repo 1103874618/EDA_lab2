1ed6ddc7848f740e823df3f33134bc0ed26133a7
kafka <a50541853@gmail.com>
2018-12-05 19:29:59 +0800

    两位十进制进位后转一位个位

--- lab2/unit_trans/unit_trans.vhd
+++ lab2/unit_trans/unit_trans.vhd
@@ -30,10 +30,23 @@ END unit_trans ;
 
 ARCHITECTURE arch_u OF unit_trans IS
 SIGNAL temp : STD_LOGIC_VECTOR(3 DOWNTO 0);
+SIGNAL a,b,c,d :STD_LOGIC ;
+
 BEGIN
+a<=u_in(3);
+b<=u_in(2);
+c<=u_in(1);
+d<=u_in(0);
 
-temp<=(3=>u_in(3),OTHERS=>u_in(3));
+m(0)<=d;
 
-m(0)<= u_in
+m(1)<= (((a AND b) OR (a AND NOT b AND c)) AND (a XOR c)) OR --这里记得,or两边的项都要加上括号,否则会报错,系统似乎分不清and和or的优先级....
+       (NOT((a AND b) OR (a AND NOT b AND c)) AND c);
+
+m(2)<= (((a AND b AND NOT c) OR (a AND NOT b AND b)) AND '0')  OR
+       ((a AND b AND c) AND '1') OR
+       (NOT((a AND b) OR (a AND NOT b AND c)) AND b);
+
+m(3)<= '0';
 
 END arch_u;
